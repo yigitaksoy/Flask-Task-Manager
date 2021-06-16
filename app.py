70bec418153933ab1a7af68c05a3e1b2b267f1c1
yigitaksoy <yigitaksoy@gmail.com>
2021-06-16 13:12:15 +0200

    Update Database

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+mongo = PyMongo(app)
 
 
-if __name__ == '__main__':
+@app.route("/")
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
+
+
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
 # Turn debug=False when deploying the project
 
